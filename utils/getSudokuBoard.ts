933b4e3c1078c78cffd2f2488df53bd0a7ce196d
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-06-17 08:44:58 +0530

    feat: New default sudoku board

--- utils/getSudokuBoard.ts
+++ utils/getSudokuBoard.ts
@@ -1,6 +1,17 @@
-const getSudokuEmptyBoard = () =>
-	Array(9)
-		.fill(null)
-		.map(() => Array(9).fill(0));
+// const getSudokuEmptyBoard = () =>
+// 	Array(9)
+// 		.fill(null)
+// 		.map(() => Array(9).fill(0));
+
+import { chunk } from "underscore";
+
+const getSudokuEmptyBoard = () => {
+	return chunk(
+		"900000073720080000000400000002003000000000000001000690009700060360009100000045002"
+			.split("")
+			.map((e) => parseInt(e)),
+		9
+	);
+};
 
 export default getSudokuEmptyBoard;
