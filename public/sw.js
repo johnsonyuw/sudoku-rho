6b84af91cb3e7aca3a91a76a683253079c202c72
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-07-04 08:28:32 +0530

    fix: service worker will be network first but will cache for offline

--- public/sw.js
+++ public/sw.js
@@ -12,26 +12,20 @@ self.addEventListener("activate", event => { });
 // NOTE: In development, we don't want to cache anything
 self.addEventListener("fetch", event => {
 	event.respondWith(
-		caches.match(event.request).then(cachedRes => {
-
-			// Cache miss - fetch the request and cache the response
-			const networkRes = fetch(event.request).then(response => {
-				// Check if we received a valid response
-				if (!response || response.status !== 200 || response.type !== "basic") {
-					return response;
-				}
-
-				// Clone the response
-				const clonedResponse = response.clone();
-
-				// Open the cache and add the cloned response
-				caches.open("pwa-cache").then(cache => {
-					cache.put(event.request, clonedResponse);
-				});
-
+		fetch(event.request).then(response => {
+			// Check if we received a valid response
+			if (!response || response.status !== 200 || response.type !== "basic") {
 				return response;
+			}
+			// Clone the response
+			const clonedResponse = response.clone();
+			// Open the cache and add the cloned response
+			caches.open("pwa-cache").then(cache => {
+				cache.put(event.request, clonedResponse);
 			});
-			return cachedRes || networkRes;
+			return response;
+		}).catch(error => {
+			return caches.match(event.request);
 		})
 	);
 });
